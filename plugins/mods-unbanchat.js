8297a33d3ffc597b4339bf5bc3bc92fbd0a516d9
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-16 09:59:42 -0400

    Actualizar mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, 'シ︎ *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '✿︎ *¡HuTao NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '🌸 *¡ NO ESTÁ BANEADA EN ESTE CHAT!🌸*', m, fake)
 chat.isBanned = false
 await conn.reply(m.chat, '❥︎ *¡🌸sumi🌸 YA FUÉ DESBANEADA EN ESTE CHAT!*', m, fake)
 }
