0dad4b2f110d64edb478dd7b7c8c423403e05af8
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-16 13:09:21 -0400

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, command }) => {
 
 let grupos =  `Hola, 
-*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 donde puedes encontrar el bot y hablar con amigos*
-*1.-* https://chat.whatsapp.com/KxHaM2J0NWPDR4RU24OmFw
+*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 donde puedes encontrar el bot oficial y hablar con amigos*
+*1.-* https://chat.whatsapp.com/GSZBZ6Ggjp02mZmZNTGQu6
 `
-await m.react('❤️‍🔥')
+await m.react('🌟')
 await conn.sendFile(m.chat, imagen1, "hutao.jpg", grupos, fkontak, null, rcanal)}
                       
 handler.command = ['grupos','linksk','gruposofc','gruposoficiales']
