c81f21fbb2d1137b330225c8cb85a3c2e18c96b6
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-16 00:19:18 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -132,7 +132,7 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nHuTao-Proyecy`;
+global.wm2 = `${dia} ${fecha}\n🌸sumi sakurazawa🌸`;
 global.nomorown = '34610246115';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
