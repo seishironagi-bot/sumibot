a455dcf081e90eb8ec0d53e4c2e44288bd77d313
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-16 12:56:11 -0400

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,7 +6,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const colombia = `Ꙭ *Mensaje:* ${pesan}`;
-  let teks = `𖣘 *INVOCANDO GRUPO*\n${colombia}\n\n𖣔 *Tags:*\n`;
+  let teks = `❤️‍🔥INVOCANDO GRUPO👀\n${colombia}\n\n𖣔 *Tags:*\n`;
   for (const mem of participants) {
     teks += `@${mem.id.split('@')[0]}\n`;
   }
