98d6ac27c0ba16074c32bea0b49ee65b75601f33
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-20 00:41:50 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ global.prems = []
 //• ↳ ◜𝑴𝑨𝑹𝑪𝑨𝑺 𝑫𝑬 𝑨𝑮𝑼𝑨◞ • 💌
 global.packsticker = '♾ ━━━━━━━━\├ ɓσƭ:\├ ρяοριєταяιο:├ ƒєϲнα ∂є ϲяєαϲιόи:├ нοяα:♾━━━━━━━━'
 global.packname = '󠁖󠁖󠁖󠁖󠁖󠁖󠁻󠁻🌸Sumi-Bot_MD🌸'
-global.author = '♾━━━━━━━━\n⇝͟͞♡̵̑ sumi-bot♡̵̑ ͟͞   ᷨᜳ ͦ ̵ͭ⋆\n𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑\n⇝ ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}\n⇝ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')} \n♾━━━━━━━━\n\n\n\nѕτιϲκєя ϐγ:'𝕯͢𝖆𝖗𝖎፝֟፝֟፝֟፝֟፝֟፝֟𝖊𝖑𝖆'
+global.author = '♾━━━━━━━━\n⇝͟͞♡̵̑ sumi-bot♡̵̑ ͟͞  
 global.wm = '🌸Sumi sakurazawa🌸';
 global.titulowm = '⍴᥆ᥕᥱ𝒓ᥱძ ᑲᥡ 𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑 𝗍ᥱᥲ𝒎☀︎';
 global.titulowm2 = '🌸Sumi sakurazawa🌸'
