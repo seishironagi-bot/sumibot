76e725a7e8911d078a124a7acbe716f86a6ee016
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-19 22:40:34 -0400

    Actualizar welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -64,7 +64,7 @@ let kick = `*╭┈⊰ *${groupMetadata.subject}* ⊰┈ ✦*
         let img3;
         try {
             img3 = await (await fetch(goodbyeImage)).buffer();
-await conn.sendFile(m.chat, img3, "Thumbnail.jpg", kick, null, fkontak)
+await conn.sendFile(m.chat, img3, "Thumbnail.jpg", kick, null,null ,fkontak)
         } catch (sendError) {
             console.error('Error al enviar mensaje de expulsión:', sendError);
         }
