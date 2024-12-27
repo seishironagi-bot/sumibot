7cf55a56ea460d5c8d22155a624fcf47521464d0
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-27 11:55:22 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -162,6 +162,8 @@ global.flaaa = [
 //* ************************
 
 //• ↳ ◜𝑨𝑷𝑰𝑺 𝑭𝑼𝑵𝑪𝑰𝑶𝑵◞ 👑
+global.MyApiRestBaseUrl = 'https://api.cafirexos.com';
+global.MyApiRestApikey = 'BrunoSobrino';
 global.openai_org_id = 'org-3';
 global.openai_key = 'sk-0';
 global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f'];
@@ -170,7 +172,7 @@ global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c7
 global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())];
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())];
-global.lolkeysapi = ['GataDiosV2']; // ['BrunoSobrino_2']
+global.lolkeysapi = ['kurumi']; // ['BrunoSobrino_2']
 global.itsrose = ['4b146102c4d500809da9d1ff'];
 
 global.APIs = {
@@ -206,10 +208,356 @@ global.APIKeys = {
   'https://api.xyroinee.xyz': 'uwgflzFEh6'
 };
 
+/** ************************/
 global.cheerio = cheerio;
 global.fs = fs;
 global.fetch = fetch;
 global.axios = axios;
 global.moment = moment;
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emot = {
+      level: '🧬 Nivel',
+      cookies: '🍪 Cookies',
+      exp: '⚡ Experiencia',
+      bank: '🏦 Banco',
+      diamond: '💎 Diamante',
+      health: '❤️ Salud',
+      kyubi: '🌀 Magia',
+      joincount: '💰 Token',
+      emerald: '💚 Esmeralda',
+      stamina: '✨ Energía',
+      role: '💪 Rango',
+      premium: '🎟️ Premium',
+      pointxp: '📧 Puntos Exp',
+      gold: '👑 Oro',
+      trash: '🗑 Basura',
+      crystal: '🔮 Cristal',
+      intelligence: '🧠 Inteligencia',
+      string: '🕸️ Cuerda',
+      keygold: '🔑 Llave de Oro',
+      keyiron: '🗝️ Llave de Hierro',
+      emas: '🪅 Piñata',
+      fishingrod: '🎣 Caña de Pescar',
+      gems: '🍀 Gemas',
+      magicwand: '⚕️ Varita Mágica',
+      mana: '🪄 Hechizo',
+      agility: '🤸‍♂️ Agilidad',
+      darkcrystal: '♠️ Cristal Oscuro',
+      iron: '⛓️ Hierro',
+      rock: '🪨 Roca',
+      potion: '🥤 Poción',
+      superior: '💼 Superior',
+      robo: '🚔 Robo',
+      upgrader: '🧰 Aumentar Mejora',
+      wood: '🪵 Madera',
+      strength: '🦹‍ ♀️ Fuerza',
+      arc: '🏹 Arco',
+      armor: '🥼 Armadura',
+      bow: '🏹 Super Arco',
+      pickaxe: '⛏️ Pico',
+      sword: '⚔️ Espada',
+      common: '📦 Caja Común',
+      uncoommon: '🥡 Caja Poco Común',
+      mythic: '🗳️ Caja Mítico',
+      legendary: '🎁 Caja Legendaria',
+      petFood: '🍖 Alimento para Mascota',
+      pet: '🍱 Caja para Mascota',
+      bibitanggur: '🍇 Semilla de Uva',
+      bibitapel: '🍎 Semilla de Manzana',
+      bibitjeruk: '🍊 Semillas de naranja',
+      bibitmangga: '🥭 Semilla de Mango',
+      bibitpisang: '🍌 Semillas de Plátano',
+      ayam: '🐓 Pollo',
+      babi: '🐖 Puerco',
+      Jabali: '🐗 Jabali',
+      bull: '🐃 Toro',
+      buaya: '🐊 Cocodrilo',
+      cat: '🐈 Gato',
+      centaur: '🐐 Centauro',
+      chicken: '🐓 Pollo',
+      cow: '🐄 Vaca',
+      dog: '🐕 Perro',
+      dragon: '🐉 Dragón',
+      elephant: '🐘 Elefante',
+      fox: '🦊 Zorro',
+      giraffe: '🦒 Jirafa',
+      griffin: '🦅 Ave',
+      horse: '🐎 Caballo',
+      kambing: '🐐 Cabra',
+      kerbau: '🐃 Búfalo',
+      lion: '🦁 León',
+      money: '❤️‍🔥 MeguCoins',
+      monyet: '🐒 Mono',
+      panda: '🐼 Panda',
+      snake: '🐍 Serpiente',
+      phonix: '🕊️ Fénix',
+      rhinoceros: '🦏 Rinoceronte',
+      wolf: '🐺 Lobo',
+      tiger: '🐅 Tigre',
+      cumi: '🦑 Calamar',
+      udang: '🦐 Camarón',
+      ikan: '🐟 Pez',
+      fideos: '🍝 Fideos',
+      ramuan: '🧪 Ingrediente NOVA',
+      knife: '🔪 Cuchillo',
+    };
+    const results = Object.keys(emot).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emot[results[0][0]];
+  }};
+global.rpgg = { // Solo emojis
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emott = {
+      level: '🧬',
+      cookies: '🍪',
+      exp: '⚡',
+      bank: '🏦',
+      diamond: '💎+',
+      health: '❤️',
+      kyubi: '🌀',
+      joincount: '💰',
+      emerald: '💚',
+      stamina: '✨',
+      role: '💪',
+      premium: '🎟️',
+      pointxp: '📧',
+      gold: '👑',
+      trash: '🗑',
+      crystal: '🔮',
+      intelligence: '🧠',
+      string: '🕸️',
+      keygold: '🔑',
+      keyiron: '🗝️',
+      emas: '🪅',
+      fishingrod: '🎣',
+      gems: '🍀',
+      magicwand: '⚕️',
+      mana: '🪄',
+      agility: '🤸‍♂️',
+      darkcrystal: '♠️',
+      iron: '⛓️',
+      rock: '🪨',
+      potion: '🥤',
+      superior: '💼',
+      robo: '🚔',
+      upgrader: '🧰',
+      wood: '🪵',
+      strength: '🦹‍ ♀️',
+      arc: '🏹',
+      armor: '🥼',
+      bow: '🏹',
+      pickaxe: '⛏️',
+      sword: '⚔️',
+      common: '📦',
+      uncoommon: '🥡',
+      mythic: '🗳️',
+      legendary: '🎁',
+      petFood: '🍖',
+      pet: '🍱',
+      bibitanggur: '🍇',
+      bibitapel: '🍎',
+      bibitjeruk: '🍊',
+      bibitmangga: '🥭',
+      bibitpisang: '🍌',
+      ayam: '🐓',
+      babi: '🐖',
+      Jabali: '🐗',
+      bull: '🐃',
+      buaya: '🐊',
+      cat: '🐈',
+      centaur: '🐐',
+      chicken: '🐓',
+      cow: '🐄',
+      dog: '🐕',
+      dragon: '🐉',
+      elephant: '🐘',
+      fox: '🦊',
+      giraffe: '🦒',
+      griffin: '🦅',
+      horse: '🐎',
+      kambing: '🐐',
+      kerbau: '🐃',
+      lion: '🦁',
+      money: '❤️‍🔥',
+      monyet: '🐒',
+      panda: '🐼',
+      snake: '🐍',
+      phonix: '🕊️',
+      rhinoceros: '🦏',
+      wolf: '🐺',
+      tiger: '🐅',
+      cumi: '🦑',
+      udang: '🦐',
+      ikan: '🐟',
+      fideos: '🍝',
+      ramuan: '🧪',
+      knife: '🔪',
+    };
+    const results = Object.keys(emott).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emott[results[0][0]];
+  }};
+global.rpgshop = { // Tienda
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emottt = {
+      exp: '⚡ Experiencia',
+      cookies: '🍪 Cookies',
+      diamond: '💎 Diamante',
+      joincount: '💰 Token',
+      emerald: '💚 Esmeralda',
+      berlian: '♦️ Joya',
+      kyubi: '🌀 Magia',
+      gold: '👑 Oro',
+      money: '❤️‍🔥 MeguCoins',
+      tiketcoin: '🎫 Megu Tickers',
+      stamina: '✨ Energía',
+      potion: '🥤 Poción',
+      aqua: '💧 Agua',
+      trash: '🗑 Basura',
+      wood: '🪵 Madera',
+      rock: '🪨 Roca',
+      batu: '🥌 Piedra',
+      string: '🕸️ Cuerda',
+      iron: '⛓️ Hierro',
+      coal: '⚱️ Carbón',
+      botol: '🍶 Botella',
+      kaleng: '🥫 Lata',
+      kardus: '🪧 Cartón',
+      eleksirb: '💡 Electricidad',
+      emasbatang: '〽️ Barra de Oro',
+      emasbiasa: '🧭 Oro Común',
+      rubah: '🦊🌫️ Zorro Grande',
+      sampah: '🗑🌫️ Super Basura',
+      serigala: '🐺🌫️ Super Lobo',
+      kayu: '🛷 Super Madera',
+      sword: '⚔️ Espada',
+      umpan: '🪱 Carnada',
+      healtmonster: '💵 Billetes',
+      emas: '🪅 Piñata',
+      pancingan: '🪝 Gancho',
+      pancing: '🎣 Caña de Pescar',
+      common: '📦 Caja Común',
+      uncoommon: '🥡 Caja Poco Común',
+      mythic: '🗳️ Caja Mítica',
+      pet: '📫 Caja de Mascotas', // ?
+      gardenboxs: '💐 Caja de Jardinería', // ?
+      legendary: '🎁 Caja Legendaria',
+      anggur: '🍇 Uva',
+      apel: '🍎 Manzana',
+      jeruk: '🍊 Naranja',
+      mangga: '🥭 Mango',
+      pisang: '🍌 Platano',
+      bibitanggur: '🌾🍇 Semillas de uva',
+      bibitapel: '🌾🍎 Semillas de manzana',
+      bibitjeruk: '🌾🍊 Semillas de naranja',
+      bibitmangga: '🌾🥭 Semillas de Mango',
+      bibitpisang: '🌾🍌 Semillas de plátano',
+      centaur: '🐐 Centauro',
+      griffin: '🦅 Ave',
+      kucing: '🐈 Gato',
+      naga: '🐉 Dragón',
+      fox: '🦊 Zorro',
+      kuda: '🐎 Caballo',
+      phonix: '🕊️ Fénix',
+      wolf: '🐺 Lobo',
+      anjing: '🐶 Perro',
+      petFood: '🍖 Alimento para Mascota', // ?
+      makanancentaur: '🐐🥩 Comida de Centauro',
+      makanangriffin: '🦅🥩 Comida de Ave',
+      makanankyubi: '🌀🥩 Comida Mágica',
+      makanannaga: '🐉🥩 Comida de Dragón',
+      makananpet: '🍱🥩 Alimentos de mascotas',
+      makananphonix: '🕊️🥩 Comida de Fénix',
+    };
+    const results = Object.keys(emottt).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emottt[results[0][0]];
+  }};
+global.rpgshopp = { // Tienda
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emotttt = {
+      exp: '⚡',
+      cookies: '🍪',
+      diamond: '💎+',
+      joincount: '💰',
+      emerald: '💚',
+      berlian: '♦️',
+      kyubi: '🌀',
+      gold: '👑',
+      money: '❤️‍🔥',
+      tiketcoin: '🎫',
+      stamina: '✨',
+      potion: '🥤',
+      aqua: '💧',
+      trash: '🗑',
+      wood: '🪵',
+      rock: '🪨',
+      batu: '🥌',
+      string: '🕸️',
+      iron: '⛓️',
+      coal: '⚱️',
+      botol: '🍶',
+      kaleng: '🥫',
+      kardus: '🪧',
+      eleksirb: '💡',
+      emasbatang: '〽️',
+      emasbiasa: '🧭',
+      rubah: '🦊🌫️',
+      sampah: '🗑🌫️',
+      serigala: '🐺🌫️',
+      kayu: '🛷',
+      sword: '⚔️',
+      umpan: '🪱',
+      healtmonster: '💵',
+      emas: '🪅',
+      pancingan: '🪝',
+      pancing: '🎣',
+      common: '📦',
+      uncoommon: '🥡',
+      mythic: '🗳️',
+      pet: '📫', // ?
+      gardenboxs: '💐', // ?
+      legendary: '🎁',
+      anggur: '🍇',
+      apel: '🍎',
+      jeruk: '🍊',
+      mangga: '🥭',
+      pisang: '🍌',
+      bibitanggur: '🌾🍇',
+      bibitapel: '🌾🍎',
+      bibitjeruk: '🌾🍊',
+      bibitmangga: '🌾🥭',
+      bibitpisang: '🌾🍌',
+      centaur: '🐐',
+      griffin: '🦅',
+      kucing: '🐈',
+      naga: '🐉',
+      fox: '🦊',
+      kuda: '🐎',
+      phonix: '🕊️',
+      wolf: '🐺',
+      anjing: '🐶',
+      petFood: '🍖', // ?
+      makanancentaur: '🐐🥩',
+      makanangriffin: '🦅🥩',
+      makanankyubi: '🌀🥩',
+      makanannaga: '🐉🥩',
+      makananpet: '🍱🥩',
+      makananphonix: '🕊️🥩',
+    };
+    const results = Object.keys(emotttt).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emotttt[results[0][0]];
+  }};
 
-     
+const file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright('Update \'megumin/config.js\''));
+  import(`${file}?update=${Date.now()}`);
+});
