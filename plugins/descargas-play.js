ad0e2b008eccf3dc62e1c14d29c0399f8a43f6fb
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-29 16:42:40 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,187 +1,228 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
-import qs from 'qs';
+import fetch from 'node-fetch'
+import yts from 'yt-search'
 
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-  if (!text) throw m.reply(`Ejemplo de uso: ${usedPrefix + command} Joji - Ew`);
-
-
-const appleMusic = {
-  search: async (query) => {
-    const url = `https://music.apple.com/us/search?term=${query}`;
+let handler = async (m, { conn: star, command, args, text, usedPrefix }) => {
+  if (!text) return conn.reply(m.chat, '*\`Ingresa El Nombre De Lo Que Quieres Buscar\`*', m, fake)
+    await m.react('🕓')
     try {
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
-        const results = [];
-        $('.desktop-search-page .section[data-testid="section-container"] .grid-item').each((index, element) => {
-            const title = $(element).find('.top-search-lockup__primary__title').text().trim();
-            const subtitle = $(element).find('.top-search-lockup__secondary').text().trim();
-            const link = $(element).find('.click-action').attr('href');
+    let res = await search(args.join(" "))
+    let img = await (await fetch(`${res[0].image}`)).buffer()
+    let txt = '*\`【Y O U T U B E - P L A Y】\`*\n\n'
+       txt += `> *\`TÍTULO:\`* ${res[0].title}\n`
+       txt += `> *\`DURACIÓN:\`* ${secondString(res[0].duration.seconds)}\n`
+       txt += `> *\`PUBLICADO:\`* ${eYear(res[0].ago)}\n`
+       txt += `> *\`CANAL:\`* ${res[0].author.name || 'Desconocido'}\n`
+       txt += `> *\`URL:\`* ${'https://youtu.be/' + res[0].videoId}\n\n`
+       txt += `> *-* _Etiqueta este mensaje con la opción a descargar 📂_\n\n*Etiqueta con \`v\` (para el video.)*\n*Etiqueta con \`a\` (para el audio.)*`
+await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['play *<text>*']
+handler.tags = ['dl']
+handler.command = ['play']
+ 
+export default handler
 
-            results.push({
-                title,
-                subtitle,
-                link
-            });
-        });
-
-        return results;
-    } catch (error) {
-        console.error("Error:", error.response ? error.response.data : error.message);
-        return { success: false, message: error.message };
-    }
-  },
-  detail: async (url) => {
-    try {
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
-        const albumTitle = $('h1[data-testid="non-editable-product-title"]').text().trim();
-        const artistName = $('a[data-testid="click-action"]').first().text().trim();
-        const releaseInfo = $('div.headings__metadata-bottom').text().trim();
-        const description = $('div[data-testid="description"]').text().trim();
-
-        const result = {
-            albumTitle,
-            artistName,
-            releaseInfo,
-            description
-        };
-
-        return result;
-    } catch (error) {
-      console.error("Error:", error.response ? error.response.data : error.message);
-      return { success: false, message: error.message };
-    }
-  }
+async function search(query, options = {}) {
+  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
+  return search.videos
 }
 
-const appledown = {
-  getData: async (urls) => {
-    const url = `https://aaplmusicdownloader.com/api/applesearch.php?url=${urls}`;
-    try {
-        const response = await axios.get(url, {
-            headers: {
-                'Accept': 'application/json, text/javascript, */*; q=0.01',
-                'X-Requested-With': 'XMLHttpRequest',
-                'User-Agent': 'MyApp/1.0',
-                'Referer': 'https://aaplmusicdownloader.com/'
-            }
-        });
-        return response.data;
-    } catch (error) {
-      return { success: false, message: error.message };
-      console.error("Error:", error.response ? error.response.data : error.message);
-    }
-  },
-  getAudio: async (trackName, artist, urlMusic, token) => {
-    const url = 'https://aaplmusicdownloader.com/api/composer/swd.php';
-    const data = {
-        song_name: trackName,
-        artist_name: artist,
-        url: urlMusic,
-        token: token
-    };
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'MyApp/1.0',
-        'Referer': 'https://aaplmusicdownloader.com/song.php#'
-    };
-    try {
-        const response = await axios.post(url, qs.stringify(data), { headers });
-        const downloadLink = response.data.dlink;
-        return downloadLink;
-    } catch (error) {
-      return { success: false, message: error.message };
-      console.error("Error:", error.response ? error.response.data : error.message);
-    }
-  },
-  download: async (urls) => {
-    const musicData = await appledown.getData(urls);
-    if (musicData) {
-        const encodedData = encodeURIComponent(JSON.stringify([
-            musicData.name,
-            musicData.albumname,
-            musicData.artist,
-            musicData.thumb,
-            musicData.duration,
-            musicData.url
-        ]));
-        const url = 'https://aaplmusicdownloader.com/song.php';
-        const headers = {
-            'authority': 'aaplmusicdownloader.com',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
-            'cache-control': 'max-age=0',
-            'content-type': 'application/x-www-form-urlencoded',
-            'origin': 'https://aaplmusicdownloader.com',
-            'referer': 'https://aaplmusicdownloader.com/',
-            'user-agent': 'MyApp/1.0'
-        };
-        const data = `data=${encodedData}`;
-        try {
-            const response = await axios.post(url, data, { headers });
-            const htmlData = response.data;
-            const $ = cheerio.load(htmlData);
-            const trackName = $('td:contains("Track Name:")').next().text();
-            const albumName = $('td:contains("Album:")').next().text();
-            const duration = $('td:contains("Duration:")').next().text();
-            const artist = $('td:contains("Artist:")').next().text();
-            const thumb = $('figure.image img').attr('src');
-            const urlMusic = urls;
-            const token = $('a#download_btn').attr('token');
-            const downloadLink = await appledown.getAudio(trackName, artist, urlMusic, token);
-
-            const extractedData = {
-                name: trackName,
-                albumname: albumName,
-                artist: artist,
-                url: urlMusic,
-                thumb: thumb,
-                duration: duration,
-                token: token,
-                download: downloadLink
-            };
-            return extractedData;
-        } catch (error) {
-          return { success: false, message: error.message };
-          console.error("Error:", error.response ? error.response.data : error.message);      
-        }
-    }
-  }
+function MilesNumber(number) {
+  let exp = /(\d)(?=(\d{3})+(?!\d))/g
+  let rep = "$1."
+  let arr = number.toString().split(".")
+  arr[0] = arr[0].replace(exp, rep)
+  return arr[1] ? arr.join(".") : arr[0]
 }
 
-conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
-
-let dataos = await appleMusic.search(text)
-let dataos2 = await appledown.download(dataos[0].link);
-let { name, albumname, artist, url, thumb, duration, token, download } = dataos2;
-
-m.reply(`_✧ Enviando ${name} (${artist}/${duration})_\n\n> ${url}`);
-      const doc = {
-      audio: { url: download },
-      mimetype: 'audio/mp4',
-      fileName: `${name}.mp3`,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          mediaType: 2,
-          mediaUrl: url,
-          title: name,
-          sourceUrl: url,
-          thumbnail: await (await conn.getFile(thumb)).data
-        }
-      }
-    };
-    await conn.sendMessage(m.chat, doc, { quoted: m });
-    await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
+function secondString(seconds) {
+  seconds = Number(seconds);
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  const dDisplay = d > 0 ? d + (d == 1 ? ' Día, ' : ' Días, ') : '';
+  const hDisplay = h > 0 ? h + (h == 1 ? ' Hora, ' : ' Horas, ') : '';
+  const mDisplay = m > 0 ? m + (m == 1 ? ' Minuto, ' : ' Minutos, ') : '';
+  const sDisplay = s > 0 ? s + (s == 1 ? ' Segundo' : ' Segundos') : '';
+  return dDisplay + hDisplay + mDisplay + sDisplay;
 }
-handler.help = ['applemusicplay'];
-handler.tags = ['downloader'];
-handler.limit = 5
-handler.command = /^(applemusicplay|play|song)$/i;
 
-export default handler;
+function sNum(num) {
+    return new Intl.NumberFormat('en-GB', { notation: "compact", compactDisplay: "short" }).format(num)
+}
+
+function eYear(txt) {
+    if (!txt) {
+        return '×'
+    }
+    if (txt.includes('month ago')) {
+        var T = txt.replace("month ago", "").trim()
+        var L = 'hace '  + T + ' mes'
+        return L
+    }
+    if (txt.includes('months ago')) {
+        var T = txt.replace("months ago", "").trim()
+        var L = 'hace ' + T + ' meses'
+        return L
+    }
+    if (txt.includes('year ago')) {
+        var T = txt.replace("year ago", "").trim()
+        var L = 'hace ' + T + ' año'
+        return L
+    }
+    if (txt.includes('years ago')) {
+        var T = txt.replace("years ago", "").trim()
+        var L = 'hace ' + T + ' años'
+        return L
+    }
+    if (txt.includes('hour ago')) {
+        var T = txt.replace("hour ago", "").trim()
+        var L = 'hace ' + T + ' hora'
+        return L
+    }
+    if (txt.includes('hours ago')) {
+        var T = txt.replace("hours ago", "").trim()
+        var L = 'hace ' + T + ' horas'
+        return L
+    }
+    if (txt.includes('minute ago')) {
+        var T = txt.replace("minute ago", "").trim()
+        var L = 'hace ' + T + ' minuto'
+        return L
+    }
+    if (txt.includes('minutes ago')) {
+        var T = txt.replace("minutes ago", "").trim()
+        var L = 'hace ' + T + ' minutos'
+        return L
+    }
+    if (txt.includes('day ago')) {
+        var T = txt.replace("day ago", "").trim()
+        var L = 'hace ' + T + ' dia'
+        return L
+    }
+    if (txt.includes('days ago')) {
+        var T = txt.replace("days ago", "").trim()
+        var L = 'hace ' + T + ' dias'
+        return L
+    }
+    return txt
+}
+
+/* import fetch from 'node-fetch'
+import yts from 'yt-search'
+
+let handler = async (m, { conn: star, command, args, text, usedPrefix }) => {
+  if (!text) return star.reply(m.chat, '🚩 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m, rcanal)
+    await m.react('🕓')
+    try {
+    let res = await search(args.join(" "))
+    let img = await (await fetch(`${res[0].image}`)).buffer()
+    let txt = '`乂  Y O U T U B E  -  P L A Y`\n\n'
+       txt += `        ✩   *Título* : ${res[0].title}\n`
+       txt += `        ✩   *Duración* : ${secondString(res[0].duration.seconds)}\n`
+       txt += `        ✩   *Publicado* : ${eYear(res[0].ago)}\n`
+       txt += `        ✩   *Canal* : ${res[0].author.name || 'Desconocido'}\n`
+       txt += `        ✩   *Url* : ${'https://youtu.be/' + res[0].videoId}\n\n`
+       txt += `> *-* Para descargar responde a este mensaje con *Video* o *Audio*.`
+await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, )
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['play *<búsqueda>*']
+handler.tags = ['downloader']
+handler.command = ['play']
+//handler.register = true 
+export default handler
+
+async function search(query, options = {}) {
+  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
+  return search.videos
+}
+
+function MilesNumber(number) {
+  let exp = /(\d)(?=(\d{3})+(?!\d))/g
+  let rep = "$1."
+  let arr = number.toString().split(".")
+  arr[0] = arr[0].replace(exp, rep)
+  return arr[1] ? arr.join(".") : arr[0]
+}
+
+function secondString(seconds) {
+  seconds = Number(seconds);
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  const dDisplay = d > 0 ? d + (d == 1 ? ' Día, ' : ' Días, ') : '';
+  const hDisplay = h > 0 ? h + (h == 1 ? ' Hora, ' : ' Horas, ') : '';
+  const mDisplay = m > 0 ? m + (m == 1 ? ' Minuto, ' : ' Minutos, ') : '';
+  const sDisplay = s > 0 ? s + (s == 1 ? ' Segundo' : ' Segundos') : '';
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+}
+
+function sNum(num) {
+    return new Intl.NumberFormat('en-GB', { notation: "compact", compactDisplay: "short" }).format(num)
+}
+
+function eYear(txt) {
+    if (!txt) {
+        return '×'
+    }
+    if (txt.includes('month ago')) {
+        var T = txt.replace("month ago", "").trim()
+        var L = 'hace '  + T + ' mes'
+        return L
+    }
+    if (txt.includes('months ago')) {
+        var T = txt.replace("months ago", "").trim()
+        var L = 'hace ' + T + ' meses'
+        return L
+    }
+    if (txt.includes('year ago')) {
+        var T = txt.replace("year ago", "").trim()
+        var L = 'hace ' + T + ' año'
+        return L
+    }
+    if (txt.includes('years ago')) {
+        var T = txt.replace("years ago", "").trim()
+        var L = 'hace ' + T + ' años'
+        return L
+    }
+    if (txt.includes('hour ago')) {
+        var T = txt.replace("hour ago", "").trim()
+        var L = 'hace ' + T + ' hora'
+        return L
+    }
+    if (txt.includes('hours ago')) {
+        var T = txt.replace("hours ago", "").trim()
+        var L = 'hace ' + T + ' horas'
+        return L
+    }
+    if (txt.includes('minute ago')) {
+        var T = txt.replace("minute ago", "").trim()
+        var L = 'hace ' + T + ' minuto'
+        return L
+    }
+    if (txt.includes('minutes ago')) {
+        var T = txt.replace("minutes ago", "").trim()
+        var L = 'hace ' + T + ' minutos'
+        return L
+    }
+    if (txt.includes('day ago')) {
+        var T = txt.replace("day ago", "").trim()
+        var L = 'hace ' + T + ' dia'
+        return L
+    }
+    if (txt.includes('days ago')) {
+        var T = txt.replace("days ago", "").trim()
+        var L = 'hace ' + T + ' dias'
+        return L
+    }
+    return txt
+} */
+        
