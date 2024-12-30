e1b5ad075a6444d35544862134b7d1593d941936
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-30 14:45:23 -0400

    Actualizar nsfw-hentai.js

--- plugins/nsfw-hentai.js
+++ plugins/nsfw-hentai.js
@@ -1,20 +1,22 @@
-import { googleImage, pinterest } from '@bochilteam/scraper'
+import { googleImage } from '@bochilteam/scraper';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!global.db.data.chats[m.chat].nsfw) return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*\n\n> Para activarlo un *Administrador* debe usar el comando */nsfw on*`, m, rcanal)
+    if (!global.db.data.chats[m.chat].nsfw) {
+        return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*\n\n> Para activarlo un *Administrador* debe usar el comando */nsfw on*`, m, rcanal);
+    }
 
-await m.react('🕓')
-try {
-let res = await (await googleImage('Imagen ' + 'hentai')).getRandom()
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', `*» Hentai*`, m, null, rcanal)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['hentai']
-handler.tags = ['nsfw']
-handler.command = ['hentai']
-handler.group = true 
-handler.register = true
-//handler.limit = 10
-export default handler
+    await m.react('🕓');
+    try {
+        let res = await googleImage('Imagen hentai');
+        let image = res[Math.floor(Math.random() * res.length)];
+        if (image && image.url) {
+            await conn.sendFile(m.chat, image.url, 'thumbnail.jpg', `*» Hentai*`, m, null, rcanal);
+            await m.react('✅');
+        } else {
+            throw new Error('No se encontró una imagen válida.');
+        }
+    } catch (e) {
+        console.error(e);
+        await m.react('✖️');
+    }
+};
