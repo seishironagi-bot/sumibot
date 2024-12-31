de0b65ef3d00d2355706c9ae1478826b349e9c12
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-31 01:14:47 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -9,10 +9,10 @@ return m.reply("❀ ingresa un texto de lo que quieres buscar")
     
 let ytres = await search(args.join(" "))
 let txt = `꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦
-✎*Título* : ${ytres[0].title}
-✎*Duración* : ${ytres[0].timestap} ✎*Publicado* : ${ytres[0].ago}
-✎*Canal* : ${ytres[0].author.name || 'Desconocido'}
-✎*Url* : ${'https://youtu.be/' + ytres[0].videoId}
+✎﹏Título• : ${ytres[0].title}
+✎﹏Duración• : ${ytres[0].timestap} ✎﹏Publicado• : ${ytres[0].ago}
+✎﹏Canal• : ${ytres[0].author.name || 'Desconocido'}
+✎﹏Url• : ${'https://youtu.be/' + ytres[0].videoId}
 ꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷`
 await conn.sendFile(m.chat, ytres[0].image, 'thumbnail.jpg', txt, m)
     
