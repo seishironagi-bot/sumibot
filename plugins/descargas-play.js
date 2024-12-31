e13bd32441dba807569e46a6bdab364bceb60910
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-31 15:26:30 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,35 +1,35 @@
-/*// *[ ❀ PLAY] *
-traer de importación de 'nodo-fetch';
-importar yts de 'yt-search'
+// *[ ❀ PLAY ]*
+import fetch from 'node-fetch'
+import yts from 'yt-search'
 
-dejar HS = async (m, {conn, texto}) => {
-si (! texto) regrese conn.reply (m.chat, `❀ Ingresa un enlace de mediafire`, m)
-dejar res = esperar yts (texto)
-deja vid = res.videos [0]
-
-intentar {
-
-let api = wait fetch (`https://api.betabotz.eu.org/api/download/ytmp4?url=${vid.url}&apikey=btzKiyoEditz`)
-deje que json = espere api.json() 
-sea { título, descripción, id, miniatura, fuente, mp3, mp4 } = json.result 
-deja audio = {
-audio: {url: mp3}, mimetype: "audio / mp4", fileName: `$ {title}`,
-contexInfo: { externalAdReply: { showAdAtribution: true, mediaType: 2,
-mediaUrl: vid.url, sourceUrl: vid.url,
-título: vid.title, cuerpo: nulo,
-thumbnailUrl: pulgar
-}}}
-espera conn.sendMessage (m.chat, audio, {quoted: m})
-
-espera conn.sendMessage (m.chat, {video: {url: mp4}, mimetype: 'video / mp4', fileName: `$ {title} .mp4`, subtítulo: nulo}, {citado: m})
+let handler = async (m, { conn, text, args }) => {
+if (!text) {
+return m.reply("❀ ingresa un texto de lo que quieres buscar")
+}
     
-} captura (error) {
-consola.error(error)
+let ytres = await search(args.join(" "))
+let txt = `꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦
+❥⏤͟͟͞͞Título♥︎ : ${ytres[0].title}
+❥⏤͟͟͞͞Duración♥︎ : ${ytres[0].timestap} ❥⏤͟͟͞͞Publicado♥︎ : ${ytres[0].ago}
+❥⏤͟͟͞͞Canal♥︎ : ${ytres[0].author.name || 'Desconocido'}
+❥⏤͟͟͞͞Url♥︎ : ${'https://youtu.be/' + ytres[0].videoId}
+꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦`
+await conn.sendFile(m.chat, ytres[0].image, 'thumbnail.jpg', txt, m)
+    
+try {
+let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp3?apikey=gifted&url=${ytres[0].url}`)
+let json = await api.json()
+let { quality, title, download_url } = json.result
+await conn.sendMessage(m.chat, { audio: { url: download_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
+} catch (error) {
+console.error(error)
 }}
 
-HS.command = ['reproducir'] 
+handler.command = /^(play)$/i
 
-exportar HS predeterminado
+export default handler
 
-
-
+async function search(query, options = {}) {
+  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
+  return search.videos
+}
