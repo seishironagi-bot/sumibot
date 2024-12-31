43ea690a4501dc74c306b9a63a7b768dad04de43
Leonel <166152300+leoneloficial@users.noreply.github.com>
2024-12-31 13:56:08 -0400

    Actualizar buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,99 +1,25 @@
 import fetch from 'node-fetch'
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `❤️‍🔥 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} megumin-bot`, m, rcanal)
-try {
-await m.react(rwait)
-const res = await fetch(global.API('https://api.github.com', '/search/repositories', {
-q: text,
-}))
-const json = await res.json()
-if (res.status !== 200) throw json
-let str = json.items.map((repo, index) => {
-return `
-💮 *Resultado:* ${1 + index}
-🔗 *Enlace:* ${repo.html_url}
-👑 *Creador:* ${repo.owner.login}
-🏵️ *Nombre:* ${repo.name}
-🫂 *Creado:* ${formatDate(repo.created_at)}
-💥 *Actualizado:* ${formatDate(repo.updated_at)}
-👀 *Visitas:* ${repo.watchers}
-✨️ *Bifurcado:* ${repo.forks}
-🌟 *Estrellas:* ${repo.stargazers_count}
-🍂 *Issues:* ${repo.open_issues}
-🍭 *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
-⭐️ *Clone:* ${repo.clone_url}
-`.trim()}).join('\n\n─────────────────\n\n') 
-// await m.react(done)
-let img = await (await fetch(json.items[0].owner.avatar_url)).buffer()
-await conn.sendMini(m.chat, '𖧷 G I T H U B - S E A R C H ❀', dev, str, img, img, redes, estilo)
-await m.react(done)
-} catch {
-await m.react(error)
-conn.reply(m.chat, '⚠︎ *No se encontró resultados de:* ' + text, m, fake)}}
-handler.help = ['githubsearch']
-handler.tags = ['buscador']
-handler.command = ['githubsearch']
-
-handler.register = false
-
-export default handler 
-
-function formatDate(n, locale = 'es') {
-const d = new Date(n)
-return d.toLocaleDateString(locale, {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'})}
-
-/*import MessageType from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
-import fs from 'fs'
-
-var handler = async (m, { conn, text, usedPrefix, command }) => {
-
-if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Ai-Yaemori`, m, rcanal)
-
-try {
-
-let res = await fetch(global.API('https://api.github.com', '/search/repositories', { q: text }))
-let json = await res.json()
-if (res.status !== 200) throw json
-let str = json.items.map((repo, index) => {
-return `
-🍟 *Resultado:* ${1 + index}
-🔗 *Enlace:* ${repo.html_url}
-👑 *Creador:* ${repo.owner.login}
-🍟 *Nombre:* ${repo.name}
-🫂 *Creado:* ${formatDate(repo.created_at)}
-💥 *Actualizado:* ${formatDate(repo.updated_at)}
-👀 *Visitas:* ${repo.watchers}
-✨️ *Bifurcado:* ${repo.forks}
-🌟 *Estrellas:* ${repo.stargazers_count}
-🍂 *Issues:* ${repo.open_issues}
-🍭 *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
-⭐️ *Clone:* ${repo.clone_url}
-`.trim()}).join('\n\n─────────────────\n\n')
-
-let img = await (await fetch(json.items[0].owner.avatar_url)).buffer()
-await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: img, thumbnailUrl: img, title: 'Resultados Encontrados 🔎',
-}
-})
-
-} catch {
-conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake)
-}
-
-}
-handler.help = ['githubsearch']
-handler.tags = ['buscador']
-handler.command = /^(githubsearch)$/i
-
+const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+let handler = async (m, { args, usedPrefix, command }) => {
+if (!args[0]) return conn.reply(m.chat, `⚠️*𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙚𝙣𝙡𝙖𝙘𝙚 𝙙𝙚 𝙂𝙞𝙩𝙝𝙪𝙗*\n• *𝙀𝙟 :* ${usedPrefix + command} ${md}`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: mg, body: ' 💫 𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🥳 ', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})    
+if (!regex.test(args[0])) return conn.reply(m.chat, `⚠️ 𝙚𝙨𝙤 𝙣𝙤 𝙚𝙨 𝙪𝙣 𝙚𝙣𝙡𝙖𝙘𝙚 𝙙𝙚 𝙜𝙞𝙩𝙝𝙪𝙗 𝙗𝙤𝙡𝙪𝙙𝙤 🤡`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: iig, body: ' 💫 𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🥳 ', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})
+try {   
+let [_, user, repo] = args[0].match(regex) || []
+repo = repo.replace(/.git$/, '')
+let url = `https://api.github.com/repos/${user}/${repo}/zipball`
+let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+conn.reply(m.chat, `*⌛ 𝐂𝐚𝐥𝐦𝐚 ✋ 𝐂𝐥𝐚𝐜𝐤, 𝐘𝐚 𝐞𝐬𝐭𝐨𝐲 𝐄𝐧𝐯𝐢𝐚𝐝𝐨 𝐞𝐥 𝐚𝐫𝐜𝐡𝐢𝐯𝐨 🚀*\n*𝐒𝐢 𝐧𝐨 𝐥𝐞 𝐥𝐥𝐞𝐠𝐚 𝐞𝐥 𝐚𝐫𝐜𝐡𝐢𝐯𝐨 𝐞𝐬 𝐝𝐞𝐛𝐢𝐝𝐨 𝐚 𝐪𝐮𝐞 𝐞𝐥 𝐑𝐞𝐩𝐨𝐬𝐢𝐭𝐨𝐫𝐢𝐨 𝐞𝐬 𝐦𝐮𝐲 𝐩𝐞𝐬𝐚𝐝𝐨*`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: ' 💫 𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🥳 ', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})   
+conn.sendFile(m.chat, url, filename, null, m, null, fake)
+handler.limit = 2
+} catch (e) { 
+m.reply(`\`\`\`⚠️ OCURRIO UN ERROR ⚠️\`\`\`\n\n> *Reporta el siguiente error a mi creador con el comando:*#report\n\n>>> ${e} <<<< `)       
+console.log(e) 
+handler.limit = 0 //❌No gastada diamante si el comando falla
+}}
+handler.help = ['gitclone <url>']
+handler.tags = ['downloader']
+handler.command = /gitclone|clonarepo|clonarrepo|repoclonar/i
 handler.register = true
-
-export default handler 
-
-function formatDate(n, locale = 'es') {
-let d = new Date(n)
-return d.toLocaleDateString(locale, {
-weekday: 'long',
-day: 'numeric',
-month: 'long',
-year: 'numeric'
-}) }*/
+//handler.limit = 2
+handler.level = 2
+export default handler
