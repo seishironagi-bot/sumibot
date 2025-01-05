843d115a8e220aceaaaab9ebf2a252135154b7a2
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-05 14:52:09 -0400

    Actualizar download-ytv.js

--- plugins/download-ytv.js
+++ plugins/download-ytv.js
@@ -1,36 +1,20 @@
-import fetch from 'node-fetch'
+// *[ ❀ YTMP4 ]*
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text }) => {
-  if (!text) {
-    return m.reply("🤍 Por favor, ingresa una URL válida de YouTube.")
-  }
-    await m.react('🕓')
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
-  let ytUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
-  if (!ytUrlRegex.test(text)) {
-    return m.reply("❀ La URL ingresada no es válida. Asegúrate de que sea un enlace de YouTube.")
-  }
+try {
+let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${text}`)
+let json = await api.json()
+let title = json.data.metadata.title
+let dl_url = json.data.download.url
+await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${json.data.filename}.mp4`, mimetype: "video/mp4" }, { quoted: m })
 
-  try {
-    let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4?apikey=gifted&url=${text}`)
-    let json = await api.json()
-    let { quality, title, download_url } = json.result
+} catch (error) {
+console.error(error)
+}}
 
-    await m.react('✅')
-    await conn.sendMessage(m.chat, { 
-      video: { url: download_url }, 
-      caption: `_${title}_`, 
-      mimetype: 'video/mp4', 
-      fileName: `${title}.mp4` 
-    }, { quoted: m })
-  } catch (error) {
-    console.error(error)
-    m.reply("❀ Hubo un error al procesar la URL. Inténtalo nuevamente.")
-  }
-}
+HS.command = ['ytmp4']
 
-handler.help = ['ytmp4 *<link yt>*']
-handler.tags = ['dl']
-handler.command = ['ytmp4', 'ytv', 'fgmp4']
-
-export default handler
+export default HS
