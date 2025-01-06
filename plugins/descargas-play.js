111255a95ed836628d7959638555429d07615421
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-06 09:15:56 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -8,11 +8,13 @@ try {
 let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${text}`)
 let json = await api.json()
 let { title, thumbnail, timestamp, ago, views, author } = json.result.metadata
-let HS = `- *Titulo :* ${title}
-- *Duracion :* ${timestamp}
-- *Subido :* ${ago}
-- *Visitas :* ${views}
-- *Autor :* ${author.name}`
+let HS = `꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦
+✿⏤͟͟͞͞Titulo✿ :* ${title}
+✿⏤͟͟͞͞Duracion✿ :* ${timestamp}
+✿⏤͟͟͞͞Subido✿ :* ${ago}
+✿⏤͟͟͞͞Visitas✿ :* ${views}
+✿⏤͟͟͞͞Autor✿ :* ${author.name}
+꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦`
 await conn.sendFile(m.chat, thumbnail, 'HasumiBotFreeCodes.jpg', HS, m)
 await conn.sendFile(m.chat, json.result.download.url, 'HasumiBotFreeCodes.mp3', null, m)
 } catch (error) {
