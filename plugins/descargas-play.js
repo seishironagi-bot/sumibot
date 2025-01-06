10c02b73c29f3f8c710b19c0a07302e06750c283
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-06 15:59:36 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,25 +1,49 @@
 // *[ ❀ PLAY ]*
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
+import yts from "yt-search";
 
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa el nombre de la cancion que quieras buscar`, m)
+let handler = async (m, { conn, text }) => {
+if (!text) {
+return m.reply("❀ Ingresa el texto de lo que quieres buscar")
+}
+
+let ytres = await yts(text)
+let video = ytres.videos[0]
+  
+if (!video) {
+return m.reply("❀ Video no encontrado")
+}
+
+let { title, thumbnail, timestamp, views, ago, url } = video
+
+let vistas = parseInt(views).toLocaleString("es-ES") + " vistas"
+
+let HS = `- *Duración:* ${timestamp}
+- *Vistas:* ${vistas}
+- *Subido:* ${ago}
+- *Enlace:* ${url}`
+
+let thumb = (await conn.getFile(thumbnail))?.data;
+
+let JT = {
+contextInfo: {
+externalAdReply: {
+title: title, body: "",
+mediaType: 1, previewType: 0,
+mediaUrl: url, sourceUrl: url,
+thumbnail: thumb, renderLargerThumbnail: true,
+}}}
+
+await conn.reply(m.chat, HS, m, JT)
 
 try {
-let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${text}`)
+let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${url}`);
 let json = await api.json()
-let { title, thumbnail, timestamp, ago, views, author } = json.result.metadata
-let HS = `⚘ᚚᩳᚚ͜ᩬᚚᷤ͜ᚚᷴ͜ᚚᷟ͜ᚚᷝ͜ᚚ͜ᚚᷤ͜ᚚᷧ͜ᚚᷜ͜ᚚᷴ͜ᚚᷢ͜ᚚᷧ͜ᚚᷦ͜ᚚᷧ͜ᚚᷱ͜ᚚᷴ͜ᚚᷧ͜ᚚᩬᚚᩳ⚘
-꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦
-✿⏤͟͟͞͞Titulo✿ : ${title}
-✿⏤͟͟͞͞Duracion✿ : ${timestamp}
-✿⏤͟͟͞͞Subido✿ : ${ago}
-✿⏤͟͟͞͞Visitas✿ : ${views}
-✿⏤͟͟͞͞Autor✿ : ${author.name}
-꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦`
-await conn.sendFile(m.chat, thumbnail, 'HasumiBotFreeCodes.jpg', HS, m)
-await conn.sendFile(m.chat, json.result.download.url, 'HasumiBotFreeCodes.mp3', null, m)
+let { download } = json.result
+
+await conn.sendMessage(m.chat, { audio: { url: download.url }, caption: ``, mimetype: "audio/mpeg", }, { quoted: m })
 } catch (error) {
-console.error(error)
+console.error(error)    
 }}
 
 handler.command = /^(play)$/i
