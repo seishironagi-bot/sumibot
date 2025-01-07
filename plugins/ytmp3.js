05edb2537a18388cc2a3213d4ff032f63054b4a0
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-07 14:39:42 -0400

    Actualizar ytmp3.js

--- plugins/ytmp3.js
+++ plugins/ytmp3.js
@@ -1,20 +1,26 @@
 // *[ ❀ YTMP3 ]*
 import fetch from 'node-fetch'
 
-let HS = async (m, { conn, text }) => {
+let handler = async (m, { conn, command, text, usedPrefix }) => {
 if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
 try {
-let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp3?url=${text}`)
+let api = await fetch(`https://axeel.my.id/api/download/audio?url=${text}`)
 let json = await api.json()
-let title = json.result.metadata.title
-let dl_url = json.result.download.url
-await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
-
+let { title, views, likes, description, author } = json.metadata
+let HS = `- *Titulo :* ${title}
+- *Descripcion :* ${description}
+- *Visitas :* ${views}
+- *Likes :* ${likes}
+- *Autor :* ${author}
+- *Tamaño :* ${json.downloads.size}
+`
+m.reply(HS)
+await conn.sendMessage(m.chat, { audio: { url: json.downloads.url }, mimetype: 'audio/mpeg' }, { quoted: m });
 } catch (error) {
 console.error(error)
 }}
 
-HS.command = ['ytmp3']
+handler.command = /^(ytmp3)$/i
 
-export default HS
+export default handler
