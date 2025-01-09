514c208373840505900bb101773f9bf8062639ed
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-08 20:39:34 -0400

    Actualizar descargas-play (2) (1).js

--- plugins/descargas-play (2) (1).js
+++ plugins/descargas-play (2) (1).js
@@ -1,34 +1,32 @@
-// *[ ❀ PLAY 2 (video) ]*
-import fetch from 'node-fetch'
+// *[ ❀ PLAY 2 ]*
+import fetch from 'node-fetch';
 import yts from 'yt-search'
 
-let handler = async (m, { conn, text, args }) => {
-if (!text) {
-return m.reply("❀ ingresa un texto de lo que quieres buscar")
-}
-    
-let ytres = await search(args.join(" "))
-let txt = `- *Título* : ${ytres[0].title}
-- *Duración* : ${ytres[0].timestamp}
-- *Publicado* : ${ytres[0].ago}
-- *Canal* : ${ytres[0].author.name || 'Desconocido'}
-- *Url* : ${'https://youtu.be/' + ytres[0].videoId}`
-await conn.sendFile(m.chat, ytres[0].image, 'thumbnail.jpg', txt, m)
-    
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa el texto de lo que quieres buscar`, m)
+let res = await yts(text)
+let vid = res.videos[0]
+
 try {
-let api = await fetch(`https://api.vreden.web.id/api/ytmp4?url=${text}`)
+
+let api = await fetch(`https://api.betabotz.eu.org/api/download/ytmp4?url=${vid.url}&apikey=btzKiyoEditz`)
 let json = await api.json()
-let { quality, title, download_url } = json.result
-await conn.sendMessage(m.chat, { video: { url: download_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
+let { title, description, id, thumb, source, mp3, mp4 } = json.result
+let audio = {
+audio: { url: mp3 }, mimetype: "audio/mp4", fileName: `${title}`,
+contextInfo: { externalAdReply: { showAdAttribution: true, mediaType: 2,
+mediaUrl: vid.url, sourceUrl: vid.url,
+title: vid.title, body: null,
+thumbnailUrl: thumb
+}}}
+await conn.sendMessage(m.chat, audio, { quoted: m })
+
+await conn.sendMessage(m.chat, { video: { url: mp4 }, mimetype: 'video/mp4', fileName: `${title}.mp4`, caption: null }, { quoted: m })    
+    
 } catch (error) {
 console.error(error)
 }}
 
-handler.command = /^(play2)$/i
+HS.command = ['play']
 
-export default handler
-
-async function search(query, options = {}) {
-  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
-  return search.videos
-}
+export default HS
