9ee7656f76ab6cbb28a32ed94ef130525e895839
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-09 00:18:39 -0400

    Actualizar descargas-play (2) (1).js

--- plugins/descargas-play (2) (1).js
+++ plugins/descargas-play (2) (1).js
@@ -8,7 +8,9 @@ return m.reply("❀ ingresa un texto de lo que quieres buscar")
 }
     
 let ytres = await search(args.join(" "))
-let txt = `- *Título* : ${ytres[0].title}
+let txt = `ᚚᚚᩳᚚ͜ᩬᚚᷤ͜ᚚᷴ͜ᚚᷟ͜ᚚᷝ͜ᚚ͜ᚚᷤ͜ᚚᷧ͜ᚚᷜ͜ᚚᷴ͜ᚚᷢ͜ᚚᷧ͜ᚚᷦ͜ᚚᷧ͜ᚚᷱ͜ᚚᷴ͜ᚚᷧ͜ᚚᩬᚚᩳᚚᚚ
+꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦
+- *Título* : ${ytres[0].title}
 - *Duración* : ${ytres[0].timestamp}
 - *Publicado* : ${ytres[0].ago}
 - *Canal* : ${ytres[0].author.name || 'Desconocido'}
