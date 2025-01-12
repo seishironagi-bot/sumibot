e1d68ac73e47e397bad702e468ba56e73f348931
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-11 23:27:26 -0400

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,30 +1,47 @@
-/* Codigo copiado de GataBot-MD */
+import { sticker } from '../lib/sticker.js'
+import axios from 'axios'
 
-import { sticker } from '../lib/sticker.js';
-import axios from 'axios';
-const handler = async (m, {conn, args, usedPrefix, command}) => {
-let text
-if (args.length >= 1) {
-text = args.slice(0).join(" ");
-} else if (m.quoted && m.quoted.text) {
-text = m.quoted.text;
-} else return conn.reply(m.chat, '⚠︎ Te Faltó El Texto!', m, rcanal);
-if (!text) return conn.reply(m.chat, '⚠︎ Te Faltó El Texto!', m, rcanal);
-const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
-const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
-const mishi = text.replace(mentionRegex, '');
-if (mishi.length > 40) return conn.reply(m.chat, '➪ El texto no puede tener mas de 30 caracteres', m, rcanal);
-const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-const nombre = await conn.getName(who)
-const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
-const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
-const buffer = Buffer.from(json.data.result.image, 'base64');
-let stiker = await sticker(buffer, false, global.packsticker, global.author);
-if (stiker) return conn.sendFile(m.chat, stiker, 'error.webp', '', m);
-}
-handler.help = ['qc'];
-handler.tags = ['sticker'];
-handler.group = true;
-handler.register = false
-handler.command = ['qc'];
-export default handler;
+let handler = async (m, { conn, text }) => {
+   if (!text) return conn.reply(m.chat, '🚩 Ingresa un texto junto al comando.', m, rcanal)
+   if (text.length > 30) return conn.reply(m.chat, 'Solo se permiten 30 caracteres como Máximo.', m, rcanal)
+   await m.react('🕓')
+   try {
+   let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => global.imgbot.noprofileurl)
+   const obj = {
+      "type": "quote",
+      "format": "png",
+      "backgroundColor": "#FFFFFF",
+      "width": 512,
+      "height": 768,
+      "scale": 2,
+      "messages": [{
+         "entities": [],
+         "avatar": true,
+         "from": {
+            "id": 1,
+            "name": m.name,
+            "photo": {
+               "url": pp
+            }
+         },
+         "text": text,
+         "replyMessage": {}
+      }]
+   }
+   const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   })
+   const buffer = Buffer.from(json.data.result.image, 'base64')
+   let stick = await sticker(buffer, false, packname, author)
+   await await conn.sendFile(m.chat, stick, 'sticker.webp', '', m)
+   await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['quotly *<texto>*']
+handler.tags = ['sticker']
+handler.command = ['quotly', 'qc']
+handler.register = true 
+export default handler
