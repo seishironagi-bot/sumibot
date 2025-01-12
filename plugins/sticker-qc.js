6e93c1209109051c2ac61d4d582b45801be372bf
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-01-11 19:32:46 -0600

    Update sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,3 +1,5 @@
+/* Codigo copiado de GataBot-MD */
+
 import { sticker } from '../lib/sticker.js';
 import axios from 'axios';
 const handler = async (m, {conn, args, usedPrefix, command}) => {
@@ -6,22 +8,23 @@ if (args.length >= 1) {
 text = args.slice(0).join(" ");
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text;
-} else return m.reply("╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇* ⊱❗️⊱╮\n\n𝘼𝙂𝙍𝙀𝙂𝙐𝙀́ 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝘾𝙍𝙀𝘼𝙍 𝙀𝙇 𝙎𝙏𝙄𝘾𝙆𝙀𝙍") 
-if (!text) return m.reply("╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇* ⊱❗️⊱╮\n\n𝘼𝙂𝙍𝙀𝙂𝙐𝙀́ 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝘾𝙍𝙀𝘼𝙍 𝙀𝙇 𝙎𝙏𝙄𝘾𝙆𝙀𝙍") 
+} else return conn.reply(m.chat, '⚠︎ Te Faltó El Texto!', m, rcanal);
+if (!text) return conn.reply(m.chat, '⚠︎ Te Faltó El Texto!', m, rcanal);
 const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
 const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
 const mishi = text.replace(mentionRegex, '');
-if (mishi.length > 45) return m.reply('*⚠️ El texto no puede tener mas de 45 caracteres*');
+if (mishi.length > 40) return conn.reply(m.chat, '➪ El texto no puede tener mas de 30 caracteres', m, rcanal);
 const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 const nombre = await conn.getName(who)
 const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
 const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
 const buffer = Buffer.from(json.data.result.image, 'base64');
-let stiker = await sticker(buffer, false, global.packname, global.author);
-if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `h`, mediaType: 2, sourceUrl: [nna, nn, md, yt].getRandom(), thumbnail: imagen4}}}, { quoted: m })
+let stiker = await sticker(buffer, false, global.packsticker, global.author);
+if (stiker) return conn.sendFile(m.chat, stiker, 'error.webp', '', m);
 }
 handler.help = ['qc'];
 handler.tags = ['sticker'];
-handler.command = /^(qc)$/i;
-handler.register = true
+handler.group = true;
+handler.register = false
+handler.command = ['qc'];
 export default handler;
