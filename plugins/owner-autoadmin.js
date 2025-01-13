4649b73938d3106cbd3392a260a9569cc14b3fd4
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-01-13 08:53:34 -0600

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -12,7 +12,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
 };
 handler.tags = ['owner'];
 handler.help = ['autoadmin'];
-handler.command = ['autoadmin , tenerpoder'];
+handler.command = ['autoadmin || tenerpoder'];
 handler.rowner = true;
 handler.group = true;
 handler.botAdmin = true;
