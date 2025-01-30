7350bfa8f64062a4260fbb80d2162d7709e066a5
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-30 00:03:18 -0400

    Actualizar herramientas-whatmusic.js

--- plugins/herramientas-whatmusic.js
+++ plugins/herramientas-whatmusic.js
@@ -1,36 +1,41 @@
+import fetch from 'node-fetch'
+import FormData from 'form-data'
 import fs from 'fs'
-import acrcloud from 'acrcloud'
-let acr = new acrcloud({
-host: 'identify-eu-west-1.acrcloud.com',
-access_key: 'c33c767d683f78bd17d4bd4991955d81',
-access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu'
-})
 
 let handler = async (m) => {
 let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
+let mime = q.mediaType || ''
 if (/audio|video/.test(mime)) {
-let media = await q.download()
-let ext = mime.split('/')[1]
-fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media)
-let res = await acr.identify(fs.readFileSync(`./tmp/${m.sender}.${ext}`))
-let { code, msg } = res.status
-if (code !== 0) throw msg
-let { title, artists, album, genres, release_date } = res.metadata.music[0]
-let txt = `
-𝚁𝙴𝚂𝚄𝙻𝚃
-• 📌 *TITLE*: ${title}
-• 👨‍🎤 𝙰𝚁𝚃𝙸𝚂𝚃: ${artists !== undefined ? artists.map(v => v.name).join(', ') : 'NOT FOUND'}
-• 💾 𝙰𝙻𝙱𝚄𝙼: ${album.name || 'NOT FOUND'}
-• 🌐 𝙶𝙴𝙽𝙴𝚁: ${genres !== undefined ? genres.map(v => v.name).join(', ') : 'NOT FOUND'}
-• 📆 RELEASE DATE: ${release_date || 'NOT FOUND'}
-`.trim()
-fs.unlinkSync(`./tmp/${m.sender}.${ext}`)
+let media = await q.download(true)
+let upload = await uploadFile(media)
+let shp = await fetch(`https://apis-starlights-team.koyeb.app/starlight/chazam?url=${upload.files[0].url}`, { headers: { 'Content-Type': 'application/json' }})
+let json = await shp.json()
+let app = { title: json.title, artist: json.artist, type: json.type, url: json.url }
+let txt = `*\`-• C H A Z A M - M U S I C •-\`*\n\n` +
+`🍟 *Nombre:* ${app.title}\n` +
+`🍟 *Artista:* ${app.artist}\n` +
+`🍟 *Tipo:* ${app.type}\n` +
+`🍟 *Link:* ${app.url}`
 m.reply(txt)
-} else throw '*𝚁𝙴𝚂𝙿𝙾𝙽𝙳 𝙰𝚄𝙳𝙸𝙾*'
-}
-
-handler.help = ['shazam']
+} else {
+throw '🚩 *Responde a un audio/video*'
+}}
+handler.help = ['chazam *<audio>*']
 handler.tags = ['tools']
-handler.command = /^quemusica|shazam|whatmusic$/i
+handler.command = /^(chazam)$/i
+handler.limit = 3
+handler.register = true
 export default handler
+
+async function uploadFile(path) {
+let form = new FormData()
+form.append('files[]', fs.createReadStream(path))
+let res = await (await fetch('https://uguu.se/upload.php', { method: 'post',
+headers: {
+...form.getHeaders()
+},
+body: form
+})).json()
+await fs.promises.unlink(path)
+return res
+}
